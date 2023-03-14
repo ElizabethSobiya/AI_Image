70a4f103e176d7f5fda4084b8a6cf7af94cea802
Elizabeth <esobiya@futuryze.com>
2023-03-14 16:55:48 +0530

    client-home-page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,29 @@
-
+import React from "react"
+import {BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Logo from './assets/logo.png'
+import {Home, CreatePost} from './page/index'
 
 function App() {
  
-
   return (
   <>
-  <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+ <BrowserRouter>
+ <header className="w-full flex justify-between items-center bg-white sm:px-8 py-4 px-4 border-b border-b-[#e6ebf4]">
+  <Link to='/'>
+  <img src={Logo} className='w-20 object-contain' alt="Logo"/>
+  </Link>
+  <Link className="font-inter font-medium text-white bg-[#6469ff] px-4 py-2 rounded-md" to='/create-post'>
+   Create
+  </Link>
+ </header>
+ <main className="sm:p-8 px-4 py-8 bg-[#f9fafe] w-full min-h-[cal(100vh-73px)]"> 
+ <Routes>
+  <Route path="/" element={<Home/>}/>
+  <Route path="/create-post" element={<CreatePost/>}/>
+ </Routes>
+
+ </main>
+ </BrowserRouter>
   </>
   )
 }
