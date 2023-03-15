11d4fb5c4ab0f244286fd28244a8ee58f2ccf0c5
Elizabeth <esobiya@futuryze.com>
2023-03-15 10:37:15 +0530

    create-post

---
+++ client/src/utilis/index.js
@@ -0,0 +1,8 @@
+import {supriseMePrompt} from '../constants '
+
+
+export function getRandomPrompt(prompt){
+    const randomIndex =
+    Math.floor(Math.random()* supriseMePrompt.length)
+    const randomPrompt = supriseMePrompt[]
+ }
