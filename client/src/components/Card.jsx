e407d3f026c390ab19a1771f48034c3864305a5d
ElizabethSobiya <ellizabethsobiya@gmail.com>
2023-11-01 23:10:28 +0530

    Search changes are added

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
-import {ImageDownload} from '../utilis';
-import DownloadIcon from '../assets/download.png'
+import React from "react";
+import { ImageDownload } from "../utilis";
+import DownloadIcon from "../assets/download.png";
 
-function Card() {
+function Card({ prompt, photo, name, _id }) {
   return (
-    <div>Card</div>
-  )
+    <div className="rounded-xl group relative  shadow-card hover:shadow-cardhover card">
+      <img
+        className="w-full h-auto rounded-xl object-cover"
+        src={photo}
+        alt={prompt}
+      />
+      <div className="group-hover:flex flex-col max-h-[94.5%] hidden absolute bottom-0 left-0 right-0 bg-[#10131f] m-2 p-4 rounded-md">
+        <p className="text-white text-md overflow-y-auto ">{prompt}</p>
+        <div className="mt-5 flex justify-between items-center gap-2">
+          <div className="flex items-center gap-2">
+            <div className="w-7 h-7 rounded-full object-cover bg-green-700 flex justify-center items-center text-white text-xs font-bold"> {name[0]}</div>
+            <p className="text-white text-sm">{name}</p>
+          </div>
+          <button type="button" onClick={()=> ImageDownload(_id, photo)} className="outline-none bg-transparent border-none"><img src={DownloadIcon} className="w-6 h-6 object-contain invert" alt="download" /></button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
